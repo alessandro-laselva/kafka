f7fdae8828080896a54eedc911e42c4cebdaeef6
alaselva <alaselva@DELL-ALASELVA.exprivia.local>
2022-07-19 12:33:34 +0200

    Add ip new server 172.30.201.127

--- kafka-consumer/src/main/java/com/kafka/KafkaApplication.java
+++ kafka-consumer/src/main/java/com/kafka/KafkaApplication.java
@@ -3,11 +3,20 @@ package com.kafka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * 
+ * @author alaselva
+ * <p>Add argument with --<name_properties=..> to change applications.properties files on cli.
+ * <p>Ex. mvn spring-boot:run -Dspring-boot.run.arguments="--spring.kafka.consumer.group.id=testcli2 --server.port=8898 --schedule.period=5000"
+ * <p>
+ */
+
 @SpringBootApplication
 public class KafkaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaApplication.class, args);
 	}
-
+	
 }
--- kafka-consumer/src/main/java/com/kafka/services/Consumer.java
+++ kafka-consumer/src/main/java/com/kafka/services/Consumer.java
@@ -1,9 +1,9 @@
-package services;
+package com.kafka.services;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-import org.springframework.validation.annotation.Validated;
+
 
 @Service
 public class Consumer {
@@ -14,7 +14,7 @@ public class Consumer {
 	@Value("${spring.kafka.consumer.group.id}")
 	String groupId;
 
-	@KafkaListener(topics = "miotopic", groupId = "${spring.kafka.consumer.group.id}")
+	@KafkaListener(topics = "${topic}", groupId = "${spring.kafka.consumer.group.id}")
 	public void consume(String message) {
 		//System.out.println("Consumer "+groupId+":\t"+message);
 		System.out.println(message);
